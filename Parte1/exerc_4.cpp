37cf9a356be8fc17c2dd8aac9198ccb403caf5e5
Hyago de Jesus <66915784+hyagoshodan@users.noreply.github.com>
2021-12-11 20:03:00 -0300

    Update exerc_4.cpp

--- Parte1/exerc_4.cpp
+++ Parte1/exerc_4.cpp
@@ -7,7 +7,9 @@ int main()
     int numero1 = 29;
     int numero2 = 30;
     int soma = numero1 + numero2;
-
+    
+    cout << "\n\nImprima a soma de dois números:\n";
+	cout << "-----------------------------------\n";
     cout << "A soma de " << numero1 << " e " << numero2 
          << " é igual a: " << soma << endl;
 
