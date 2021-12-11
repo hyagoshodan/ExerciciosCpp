0c5f4bb54b2076c715613931be91d35e3077cca0
hyagoshodan <hyagodejesus@outlook.com>
2021-12-11 19:49:56 -0300

    Segundo exercicio - Completo

---
+++ Parte1/exerc_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero1 = 29;
+    int numero2 = 30;
+    int soma = numero1 + numero2;
+
+    cout << "A soma de " << numero1 << " e " << numero2 
+         << " é igual a: " << soma << endl;
+
+    return 0;        
+}
