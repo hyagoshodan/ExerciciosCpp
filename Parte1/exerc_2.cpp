ae33fe5d23c34af473d64f1ec8f5462c357f47e7
hyagoshodan <hyagodejesus@outlook.com>
2021-12-11 19:59:02 -0300

    Exercicio 4 - Completo

--- Parte1/exerc_2.cpp
+++ Parte1/exerc_2.cpp
@@ -4,12 +4,7 @@ using namespace std;
 
 int main()
 {
-    int numero1 = 29;
-    int numero2 = 30;
-    int soma = numero1 + numero2;
-
-    cout << "A soma de " << numero1 << " e " << numero2 
-         << " é igual a: " << soma << endl;
+    cout << "A soma de 29 e 30 é igual a: 59\n";
 
     return 0;        
 }
