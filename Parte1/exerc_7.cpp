99c3cae541e5311988b9571897653401d8c134bc
hyagoshodan <hyagodejesus@outlook.com>
2021-12-12 20:45:15 -0300

    Setimo exercicio - Corrigido

--- Parte1/exerc_7.cpp
+++ Parte1/exerc_7.cpp
@@ -5,20 +5,49 @@ using namespace std;
 
 int main()
 {
+ /*                 Primeira tentativa - errada.
+  *  cout << "\nExibir operações aritméticas com tipos de dados mistos:\n";
+  *  cout << "---------------------------------------------------------\n";
+  *  cout << "5 + 7 = "      << 5 + 7        << endl;
+  *  cout << "3.7 + 8.0 = "  << 3.7 + 8.0    << endl;
+  *  cout << "5 + 8.0 = "    << 5 + 8.0      << endl;
+  *
+  *  cout << "5 - 7 = "      << 5 - 7        << endl;
+  *  cout << "3.7 - 8.0 = "  << 3.7 - 8.0    << endl;
+  *  cout << "5 - 8.0 = "    << 5 - 8.0      << endl;
+  *
+  *  cout << "5 * 7 = "      << 5 * 7        << endl;
+  *  cout << "3.7 * 8.0 = "  << 3.7 * 8.0    << endl;
+  *  cout << "5 * 8.0 = "    << 5 * 8.0      << endl;
+  *
+  *  cout << "5 / 7 = "      << 5 / 7        << endl;
+  *  cout << "3.7 / 8.0 = "  << 3.7 / 8.0    << endl;
+  *  cout << "5 / 8.0 = "    << 5 / 8.0      << endl;
+  */
+
+    float oito = 8.0;
+    float tres_sete = 3.7;
+    int sete = 7;
+    int cinco = 5;
+
+
     cout << "\nExibir operações aritméticas com tipos de dados mistos:\n";
     cout << "---------------------------------------------------------\n";
-    cout << "5 + 7 = "      << 5 + 7        << endl;
-    cout << "3.7 + 8.0 = "  << 3.7 + 8.0    << endl;
-    cout << "5 + 8.0 = "    << 5 + 8.0      << endl;
-    cout << "5 - 7 = "      << 5 - 7        << endl;
-    cout << "3.7 - 8.0 = "  << 3.7 - 8.0    << endl;
-    cout << "5 - 8.0 = "    << 5 - 8.0      << endl;
-    cout << "5 * 7 = "      << 5 * 7        << endl;
-    cout << "3.7 * 8.0 = "  << 3.7 * 8.0    << endl;
-    cout << "5 * 8.0 = "    << 5 * 8.0      << endl;
-    cout << "5 / 7 = "      << 5 / 7        << endl;
-    cout << "3.7 / 8.0 = "  << 3.7 / 8.0    << endl;
-    cout << "5 / 8.0 = "    << 5 / 8.0      << endl;
+    cout << cinco      << " + " << sete << " = " << cinco + sete      << endl;
+    cout << tres_sete  << " + " << oito << " = " << tres_sete + oito  << endl;
+    cout << cinco      << " + " << oito << " = " << cinco + oito      << endl;
+    
+    cout << cinco      << " - " << sete << " = " << cinco - oito      << endl;
+    cout << tres_sete  << " - " << oito << " = " << tres_sete - oito  << endl;
+    cout << cinco      << " - " << oito << " = " << cinco - oito      << endl;
 
+    cout << cinco      << " * " << sete << " = " << cinco * sete      << endl;
+    cout << tres_sete  << " * " << oito << " = " << tres_sete * oito  << endl;
+    cout << cinco      << " * " << oito << " = " << cinco * oito      << endl;
+
+    cout << cinco      << " / " << sete << " = " << cinco / sete      << endl;
+    cout << tres_sete  << " / " << oito << " = " << tres_sete / oito  << endl;
+    cout << cinco      << " / " << oito << " = " << cinco / oito      << endl;
+    
     return 0;
 }
