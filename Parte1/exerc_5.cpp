5599f409c2e86de67e1f01b32de8731d91a98062
hyagoshodan <hyagodejesus@outlook.com>
2021-12-12 10:04:48 -0300

    Quinto exercicio - Completo

---
+++ Parte1/exerc_5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "\nO limite máximo do tipo de dados int: 2147483647\n"
+         << "O limite mínimo do tipo de dados int: -2147483648\n"
+         << "O limite máximo de tipo de dados int não assinado: 4294967295\n"
+         << "O limite máximo de long tipo de dados long: 9223372036854775807\n"
+         << "O limite mínimo do tipo de dados long long: -9223372036854775808\n"
+         << "O limite máximo de tipo de dados long long unsigned: 18446744073709551615\n"
+         << "Os bits contêm no tipo de dados char: 8\n"
+         << "O limite máximo do tipo de dados char: 127\n"
+         << "O limite mínimo do tipo de dados char: -128\n"
+         << "O limite máximo do tipo de dados char assinado: 127\n"
+         << "O limite mínimo do tipo de dados char assinado: -128\n"
+         << "O limite máximo do tipo de dados unsigned char: 255\n"
+         << "O limite mínimo do tipo de dados curtos: - 32768\n"
+         << "O limite máximo do tipo de dados curtos: 32767\n"
+         << "O limite máximo do tipo de dados curtos não assinados: 65535\n"
+         << endl;
+
+    return 0;
+}
