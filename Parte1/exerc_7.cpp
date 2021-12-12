1e1ec74b7d235a12de1f1d1c48b0fb1ef2500854
hyagoshodan <hyagodejesus@outlook.com>
2021-12-12 20:25:47 -0300

    Setimo exercicio - Completo

---
+++ Parte1/exerc_7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nExibir operações aritméticas com tipos de dados mistos:\n";
+    cout << "---------------------------------------------------------\n";
+    cout << "5 + 7 = "      << 5 + 7        << endl;
+    cout << "3.7 + 8.0 = "  << 3.7 + 8.0    << endl;
+    cout << "5 + 8.0 = "    << 5 + 8.0      << endl;
+    cout << "5 - 7 = "      << 5 - 7        << endl;
+    cout << "3.7 - 8.0 = "  << 3.7 - 8.0    << endl;
+    cout << "5 - 8.0 = "    << 5 - 8.0      << endl;
+    cout << "5 * 7 = "      << 5 * 7        << endl;
+    cout << "3.7 * 8.0 = "  << 3.7 * 8.0    << endl;
+    cout << "5 * 8.0 = "    << 5 * 8.0      << endl;
+    cout << "5 / 7 = "      << 5 / 7        << endl;
+    cout << "3.7 / 8.0 = "  << 3.7 / 8.0    << endl;
+    cout << "5 / 8.0 = "    << 5 / 8.0      << endl;
+
+    return 0;
+}
