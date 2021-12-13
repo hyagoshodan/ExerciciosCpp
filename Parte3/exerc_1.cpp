2c669903eb3196dde684482e114297208a2ace96
hyagoshodan <hyagodejesus@outlook.com>
2021-12-13 17:03:41 -0300

    Parte 3 - Exercicio 1. Completo

--- Parte3/exerc_1.cpp
+++ Parte3/exerc_1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+
+    int contagem = 1;
+
+    cout << "Os números naturais são:\n";
+
+        while( contagem <= 10 )
+        {
+            cout << contagem << " ";
+            contagem = contagem + 1;        
+        }    
+    
+    cout << "\n";
+
+    return 0;
+}
