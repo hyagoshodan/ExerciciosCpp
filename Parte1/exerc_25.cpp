b8ec99792460dcefe44c8e418862d8c3ecabacbc
hyagoshodan <hyagodejesus@outlook.com>
2021-12-14 23:16:29 -0300

    Modificando exercicio 25 - Parte 1

--- Parte1/exerc_25.cpp
+++ Parte1/exerc_25.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main()
 {
     int kelvin;
+    double celsius;
 
     cout << "Converta a temperatura em Kelvin para Celsius:\n";
     cout << "------------------------------------------------\n";
+    
     cout << "Insira a temperatura em Kelvin: ";
     cin >> kelvin;
 
+    celsius = kelvin - 273.15;
+
     cout << "A temperatura em Kelvin: " << kelvin << endl;
-    cout << "A temperatura em Celsius: " << kelvin - 273.15 << endl;
+    cout << "A temperatura em Celsius: " << celsius << endl;
     return 0; 
 }
