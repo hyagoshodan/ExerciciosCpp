bdfb36d75701c5aa77559a386e630ab4d72e1e73
hyagoshodan <hyagodejesus@outlook.com>
2021-12-16 15:43:06 -0300

    Exercicio 39 - Parte 1

---
+++ Parte1/exerc_39.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " xxxxx" << endl;
+    cout << "x     x       x        x" << endl;
+    cout << "x             x        x" << endl;
+    cout << "x          xxxxxxx  xxxxxxx" << endl;
+    cout << "x             x        x" << endl;
+    cout << "x     x       x        x" << endl;
+    cout << " xxxxx" << endl;
+
+    return 0;
+}
