31bc5dd9a475878213b1386397198fd03627b2f6
hyagoshodan <hyagodejesus@outlook.com>
2021-12-19 22:23:55 -0300

    Parte 1 - exercicio 17 incompleto

---
+++ Parte1/exerc_17.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int comprimento;
+    int largura;
+
+    cout << "Insira o comprimento do retângulo: ";
+    cin >> comprimento;
+
+    cout << "Insira a largura do retângulo: ";
+    cin >> largura;
+
+    cout << "A área do retângulo é: " << endl;
+    cout << "O perímetro do retângulo é: " << endl;
+
+    
+
+    return 0;
+}
