9b0ef64949843411fc16642ac24c804f488340c8
hyagoshodan <hyagodejesus@outlook.com>
2022-01-03 23:02:44 -0300

    Exercicio 13 - Parte1

---
+++ Parte1/exerc_13.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int numero1, numero2;
+
+    cout << "Insira o primeiro número: ";
+    cin >> numero1;
+
+    cout << "Insira o segundo número: ";
+    cin >> numero2;
+
+    cout << "Após a troca o primeiro número é: " << numero2 << endl;
+    cout << "Após a troca o segundo número é: " << numero1 << endl;
+
+    return 0;
+}
