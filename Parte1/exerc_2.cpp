2cfdaa8da06cd18fb6c4369d43f5c84f6893c528
Hyago de Jesus <66915784+hyagoshodan@users.noreply.github.com>
2022-01-06 20:59:30 -0300

    Update exerc_2.cpp

--- Parte1/exerc_2.cpp
+++ Parte1/exerc_2.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "\n\nImprima a soma de dois números:\n";
     cout << "-----------------------------------\n";
-    cout << "A soma de 29 e 30 é igual a: 59\n";
+    cout << "A soma de 29 e 30 é igual a:" << 29 + 30 << "\n";
 
     return 0;        
 }
